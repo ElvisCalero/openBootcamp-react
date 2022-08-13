0eab5488543cc0bacbd667cf4acebc0eb1a3afb8
Elvis Calero <elviscalero962010@gmail.com>
2022-08-12 23:59:49 -0500

    class 6
    using css on the components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import GreetingStyled from './components/pure/greetingStyled';
 // import Greeting from './components/pure/Greeting';
 // import GreetingF from './components/pure/GreetingF';
 // import TaskListComponent from './components/containers/task_list';
 // import ContactListComponent from './components/containers/contact_list';
 // import Ejemplo1 from './hooks/Ejemplo1';
-//import Ejemplo2 from './hooks/Ejemplo2';
-import ComponentWithContext from './hooks/Ejemplo3';
-import Ejemplo4 from './hooks/Ejemplo4';
+// import Ejemplo2 from './hooks/Ejemplo2';
+// import ComponentWithContext from './hooks/Ejemplo3';
+// import Ejemplo4 from './hooks/Ejemplo4';
 
 
 function App() {
@@ -21,9 +22,10 @@ function App() {
         {/* <Ejemplo1></Ejemplo1> */}
         {/* <Ejemplo2></Ejemplo2> */}
         {/* <ComponentWithContext></ComponentWithContext> */}
-        <Ejemplo4 name="Elvis">
+        {/* <Ejemplo4 name="Elvis">
           <h3>Contenido del props.children</h3>
-        </Ejemplo4>
+        </Ejemplo4> */}
+        <GreetingStyled name="Elvis"></GreetingStyled>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/pure/greetingStyled.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const loggedStyle = {
+    color: 'white'
+}
+const unloggedStyle = {
+    color: 'tomato',
+    fontWeight: 'bold'
+}
+const GreetingStyled = (props) => {
+    const [logged, setLogged] = useState(false)
+    return (
+        <div style={logged ? loggedStyle : unloggedStyle}>
+            {
+                logged ? 
+                <p>Hola, {props.name}</p>
+                :
+                <p>Please login</p>
+            }
+            <button onClick={() => {
+                console.log('Boton pulsado');
+                setLogged(!logged);
+            }}>{logged ? 'Logout' : 'Login'}</button>
+        </div>
+    );
+}
+
+export default GreetingStyled;
